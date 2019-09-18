93f97b9d90425a615001cb2d7b4d5cdd5c596601
Stanley-Okwii <stanleeparker12@gmail.com>
2019-09-18 15:26:26 +0300

    Add dockerization of the application

--- authors/apps/core/registration_utils.py
+++ authors/apps/core/registration_utils.py
@@ -97,12 +97,12 @@ def send_mail_user(request, serializer):
     email = user['email']
     body = "Click here to verify your account {}/api/users/verify/?token={}"
 
-    send_mail('Email-verification',
-              body
-              .format(url_param, serializer.data['token']),
-              settings.EMAIL_HOST_USER,
-              [email],
-              fail_silently=False,)
+    # send_mail('Email-verification',
+    #           body
+    #           .format(url_param, serializer.data['token']),
+    #           settings.EMAIL_HOST_USER,
+    #           [email],
+    #           fail_silently=False,)
     info = "You have successfully been registered, \
            please check your email for confirmation"
     refined_info = re.sub('  +', ' ', info)
